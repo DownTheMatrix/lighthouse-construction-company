151614d0b087257e256c752f8bf36a932b3afb1d
DownTheMatrix <suija87@gmail.com>
2019-03-12 17:08:56 +0100

    Code refactor

--- js/main.js
+++ js/main.js
@@ -30,20 +30,27 @@ const init = () => {
   const servicesLink = document.querySelector('a[href="#services"]');
   const branchesLink = document.querySelector('a[href="#branches"]');
   const teamLink = document.querySelector('a[href="#team"]');
-  const contactLink = document.querySelector('a[href="#contact"]');
+  const contactLink = document.querySelector('a[href="#cooperation"]');
 
   homeLink.addEventListener("click", () => smoothScroll("#header", 1000));
   servicesLink.addEventListener("click", () => smoothScroll("#services", 1000));
   branchesLink.addEventListener("click", () => smoothScroll("#branches", 1000));
   teamLink.addEventListener("click", () => smoothScroll("#team", 1000));
-  contactLink.addEventListener("click", () => smoothScroll("#contact", 1000));
+  contactLink.addEventListener("click", () => smoothScroll("#cooperation", 1000));
 
   /* Toggle Modal Box */
   const modalBtn = document.querySelector(".modal-btn");
+  const modalBOx = document.querySelector("#modal-box");
+  const docBody = document.querySelector("body"); 
+  const closeBtn = document.querySelector(".close-btn");
 
-  modalBtn.addEventListener("click", () => {
-    
-  });
+  const toggleModal = () => {
+    modalBOx.classList.toggle("modal-show");
+    docBody.classList.toggle("body-overflow");
+  };
+
+  modalBtn.addEventListener("click", toggleModal);
+  closeBtn.addEventListener("click", toggleModal);
 };
 
 document.addEventListener("DOMContentLoaded", init);
